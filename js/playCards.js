9b57095306cf8f476d4c73c1c89ca6e926db0b1f
Dango916 <thomasa916@gmail.com>
2015-01-27 15:38:40 -0800

    Update playCards.js

--- js/playCards.js
+++ js/playCards.js
@@ -16,6 +16,10 @@ $(document).ready(function(){
         for(var i=0;i<hand.length;i++){
             el.append(hand[i].getHTML());
         }
+    }
+        var showHand = function(){
+        var el = $('#theirHand')
+        el.html('');
         for(var i=0;i<hand1.length;i++){
             el.append(hand1[i].getHTML());
         }
